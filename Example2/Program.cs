c84bb8fee6243de10bb9c451ac673b0ed83264ce
Safura234 <safuramg@code.edu.az>
2022-05-17 13:16:00 +0400

    https://github.com/Safura234/Try---catch-examples.git

---
+++ Example2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter fullName to register:");
+                string fullName = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred: {0}");
+            }
+            finally
+            {
+                Console.Write("Enter fullName, name and surname:");
+            }
+            }
+           
+        }
+    }
+
